25421e936954380bd83e1819d9add4f96c9074eb
mdeter777 <eter_wheyy@proton.me>
2025-10-17 19:14:21 +0200

    quick commit

--- src/mini3d.c
+++ src/mini3d.c
@@ -606,12 +606,15 @@ int main(int argc, char* argv[])
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 
+    // set the texture wrapping/filtering options 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_width, tex_height, 0, 
                                 GL_RGBA, GL_UNSIGNED_BYTE, texture_data);
     glGenerateMipmap(GL_TEXTURE_2D);
-
-    
-    
 
     stbi_image_free(texture_data);
 
