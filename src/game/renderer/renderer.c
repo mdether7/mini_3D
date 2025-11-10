e7f46d54c8b97c08f3b8920352ffc404c56d2d96
mdeter777 <eter_wheyy@proton.me>
2025-11-10 18:50:12 +0100

    Quick commit on 2025-11-10 18:50:12

--- src/game/renderer/renderer.c
+++ src/game/renderer/renderer.c
@@ -3,6 +3,8 @@
 #include <glad/glad.h>
 #include <linmath/linmath.h>
 
+#include "shader.h"
+
 // Cube with minimal data for displaying textures / using normals.
 static float dg3d_renderer_vertex_data_cube[] = {
     
@@ -42,7 +44,7 @@ static float dg3d_renderer_vertex_data_cube[] = {
 
 static unsigned int dg3d_renderer_index_data_cube[] = {
 
-    // CUBE //
+    // CUBE //////
     // Front face
     0, 1, 2,
     2, 3, 0,
@@ -61,6 +63,7 @@ static unsigned int dg3d_renderer_index_data_cube[] = {
     // Bottom face
     20, 21, 22,
     22, 23, 20,
+    ////////////// 
 };
 
 
@@ -70,35 +73,65 @@ static unsigned int dg3d_renderer_index_data_cube[] = {
 // 1,0,
 // 1,1
 
-typedef struct {
-    float*        vertices;
-    unsigned int* indices;
-} DG3D_Model;
+// void glDrawArrays( 	GLenum mode,
+//   	GLint first,
+//   	GLsizei count);
+// first specifies per vertex offset (not byte offset) (LIVES IN VBO)
+
+// void glDrawElements( 	GLenum mode,
+//   	GLsizei count,
+//   	GLenum type,
+//   	const void * indices);
+// indices specifies index per byte offset (LIVES IN EBO)
+
 
 typedef struct {
-    GLuint id_gl;
+    GLuint        VAO;
+    GLuint        VBO;
+    GLuint        EBO;
+} DG3D_Mesh;
+
+typedef struct {
+    DG3D_Shader shader;
     //? NOTE: Maybe add source remembering or someshit?? 
-    // locations as needed...
     GLint u_model;
     GLint u_time;
     GLint u_res;
-} DG3D_DeafultShader; 
+} DG3D_DefaultShader; 
 
 typedef struct {
-    DG3D_DeafultShader shader_default;
-    DG3D_Model cube;
+
+    DG3D_DefaultShader shader_default;
+
+    GLuint FBO;
+
+    int viewport_width;
+    int viewport_height;
+    
 } DG3D_Renderer;
 
-DG3D_Renderer renderer;
-
-int dg3d_renderer_init(void)
+int dg3d_renderer_init(DG3D_Renderer* renderer)
 {   
+    glGenFramebuffers(1, &renderer->FBO);
+
+    // init shaders.
+    renderer->shader_default.shader = shader_program_compile_from_path("shaders/dg3d_default.vert", "shaders/dg3d_default.frag");
+    if (renderer->shader_default.shader.id == 0) return 1;
 
 
 
 }
 
-void dg3d_renderer_shutdown(void)
+void dg3d_renderer_shutdown(DG3D_Renderer* renderer)
+{
+    // Shaders Cleanup
+    shader_program_delete(&renderer->shader_default.shader);
+
+    // Buffer Cleanup
+    glDeleteFramebuffers(1, &renderer->FBO);
+}
+
+void dg3d_render_mesh(DG3D_Mesh* mesh, vec3 pos)
 {
 
 }
